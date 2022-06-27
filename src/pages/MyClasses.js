0b3f04cbb2952a7ef546ca0f02c838bce78cda6e
iisalahabdulla <46461385+iisalahabdulla@users.noreply.github.com>
2022-06-28 00:16:45 +0300

    latest

--- src/pages/MyClasses.js
+++ src/pages/MyClasses.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from '../components/Navbar';
 
 const MyClasses = () => {
+
+    useEffect(() => {
+        
+    },[]);
+
   return (
     <>
       <Navbar />
